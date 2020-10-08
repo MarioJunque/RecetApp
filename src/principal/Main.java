f929572dc238c69bc7b349803b4811688713da93
Sergio <sergiolopez382@gmail.com>
2020-10-08 17:49:28 +0200

    Juntadas las dos ramas en la rama master y mejora del login en Java

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -19,10 +19,23 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
+        String usuario= "User1";
+        String contrasena = "1234";
 
         System.out.println("\nPor favor, introduzca su nombre de usuario:\n");
         String user_name = scanner.nextLine();
-        System.out.println("\nPor favor, introduzca su contraseña:\n");
-        String pass = scanner.nextLine();
+        if(user_name.equals(usuario)){
+            System.out.println("\nPor favor, introduzca su contraseña:\n");
+            String pass = scanner.nextLine();
+                if(pass.equals(contrasena)){
+                    System.out.println("\n Acceso concedido\n");
+                }
+                else{
+                    System.out.println("\nContraseña incorrecto\n");
+                }
+        }
+        else{
+            System.out.println("\nUsuario incorrecto\n");
+        }
     }
 }
