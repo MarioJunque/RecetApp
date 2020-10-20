3715307056fcc673ef82388531dd6a8cf49e8458
Sergio <sergiolopez382@gmail.com>
2020-10-20 19:50:09 +0200

    Arreglados los test, ya funciona bien el build

--- golang/Login_test.go
+++ golang/Login_test.go
@@ -1,10 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"github.com/MarioJunque/RecetApp/tree/master/golang/login"
+)
 
 func TestGuardaNombre(t *testing.T) {
     expected := "El usuario es correcto, accediendo ..."
-    got := IngresarCredenciales("Paco", "caca123")
+    got := login.IngresarCredenciales("Paco", "caca123")
 
     if expected != got {
         t.Errorf("got '%s' expected '%s'", got, expected)
