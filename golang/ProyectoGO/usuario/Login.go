1df12339e3656213c91c7266cf255185665d668e
QuiqueCollada <e.collada@usp.ceu.es>
2020-10-22 20:10:35 +0200

    Login con MariaDB y autenticación

--- golang/ProyectoGO/usuario/Login.go
+++ golang/ProyectoGO/usuario/Login.go
@@ -1,48 +1,93 @@
 package usuario
 import (
 		"fmt"
-		"database/sql"
+				"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 var db *sql.DB
 var err error
 
+//type Usuario struct {
+//correo   string
+//nombre   string
+//password string
+//}
+//var u Usuario
 
 
 func Login() string {
 		var usuario string
 		var contrasenna string
 
+
 		fmt.Println("Introduce el nombre de usuario:")
 		fmt.Scanln(&usuario)
+		u.nombre = usuario
 		fmt.Println("Ahora Introduce la contraseña:")
 		fmt.Scanln(&contrasenna)
+		u.password = contrasenna
 
-		resultado := IngresarCredenciales(usuario, contrasenna)
+		resultado := IngresarCredenciales(u)
 		fmt.Println(resultado)
 		return resultado
 }
 
-func obtenerBaseDeDatos() (db *sql.DB, e error) {
-		usuario := "root"
-		pass := "password"
-		host := "localhost"
-		nombreBaseDeDatos := "agenda"
-		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", usuario, pass, host, nombreBaseDeDatos))
-		if err != nil {
-				return nil, err
-		}
-		return db, nil
+
+func autenticar(db *sql.DB, nombre string) (string, error){
+
+	var pass string
+	stmt, err := db.Prepare("SELECT contraseña FROM usuario WHERE nombre = ?")
+	if err != nil {
+		return "NOK", err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(nombre)
+	if err != nil {
+		return "NOK", err
+	}
+	defer rows.Close()
+	for rows.Next() {
+        if err := rows.Scan(&pass); err != nil {
+                log.Fatal(err)
+        }
+        fmt.Printf("%s is %d\n", pass, nombre)
+	}
+	if err := rows.Err(); err != nil {
+        log.Fatal(err)
 }
 
-func IngresarCredenciales(user string, password string) string {
-        usuarioVerificado := "Paco"
-		passwordVerificada := "caca123"
+//	return res.LastInsertId()
+	return pass, err
+}
+
+//func IngresarCredenciales(user string, password string) string {
+//        usuarioVerificado := "Paco"
+//		passwordVerificada := "caca123"
+
+
+	func IngresarCredenciales(user Usuario) string {
 
 		usuarioConfirmado := "El usuario es correcto, accediendo ..."
 		usuarioIncorrecto := "Usuario no valido, vuelva a intentarlo"
 
-		if user == usuarioVerificado && password == passwordVerificada {
+	db, err := sql.Open("mysql", "root:root@/recetapp")
+	if err != nil {
+		log.Fatal("Cannot open DB connection", err)
+	}
+	defer db.Close()
+
+	passwordBBDD, err := autenticar(db, user.nombre)
+	if err != nil {
+		log.Fatal("Failed to insert into database", err)
+	}
+	log.Printf("Inserted row with ID of: %d\n", passwordBBDD)
+
+
+
+		if  user.password == passwordBBDD {
 				return usuarioConfirmado
     } else {
 				return usuarioIncorrecto
