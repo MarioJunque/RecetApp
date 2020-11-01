6405461e2b1d16e932e43370ddd10c8d53fae998
QuiqueCollada <e.collada@usp.ceu.es>
2020-11-01 18:56:11 +0100

    Función para devolver todos los ingredientes que ha seleccionado el cliente

--- golang/ProyectoGO/recetas/Ingrediente.go
+++ golang/ProyectoGO/recetas/Ingrediente.go
@@ -84,3 +84,43 @@ func Insert(db *sql.DB, ingrediente Ingrediente, id_usuario int) (int64, error) 
 
 	return res.LastInsertId()
 }
+
+func MostrarMisIngredientes(id_usuario int) {
+
+
+
+}
+func ObtenerMisIngredientes(db *sql.DB, id_usuario int) ([]Ingrediente, error) {
+
+//	var ingrediente Ingrediente
+//	stmt := "select id_usuarios, id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios=?;"
+//	row := db.QueryRow(stmt, id_usuario)
+//	err := row.Scan(&u.id_usuario,&ingrediente.id_ingrediente,&ingrediente.nombre)
+//	switch err {
+//	case sql.ErrNoRows:
+		//  	fmt.Println("No está disponible este ingrediente en la base de datos")
+//		return -1,-1, "NOK" err
+//	case nil:
+//		return u.id_usuario, ingrediente.id_ingrediente, ingrediente.nombre, err
+//	default:
+//		panic(err)
+//	}
+    rows, err := db.Query("select id_usuarios, id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios=?;")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        err := rows.Scan(&u.id_usuario,&ingrediente.id_ingrediente,&ingrediente.nombre)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        ingredientes = append(ingredientes, User{id_ingrediente: id_ingredientes, nombre: nombre})
+    }
+    if err := rows.Err(); err != nil {
+        log.Fatal(err)
+    } return ingredientes, err
+}
+
