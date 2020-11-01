68fe69bd52dee1a503dfcbe0a8696cc7e9664637
Sergio <sergiolopez382@gmail.com>
2020-11-01 18:49:13 +0100

    Update de las tareas y añadida la opción que permite al usuario acceder al Login si se equivoca en el primer intento

--- golang/ProyectoGO/Main.go
+++ golang/ProyectoGO/Main.go
@@ -2,46 +2,50 @@ package main
 
 import (
 	"fmt"
-	"github.com/MarioJunque/RecetApp/tree/master/golang/ProyectoGO/usuario"
-	"github.com/MarioJunque/RecetApp/tree/master/golang/ProyectoGO/recetas"
-)
 
+	"github.com/MarioJunque/RecetApp/tree/master/golang/ProyectoGO/recetas"
+	"github.com/MarioJunque/RecetApp/tree/master/golang/ProyectoGO/usuario"
+)
 
 func main() {
 
-		var opcion int
-		for{
-			fmt.Println("Ecriba\n 1 para registrarse como usuario\n 2 para hacer login\n 3 para salir de la aplicación")
-			fmt.Scanln(&opcion)
+	var opcion int
+	for {
+		fmt.Println("Ecriba\n 1 para registrarse como usuario\n 2 para hacer login\n 3 para salir de la aplicación")
+		fmt.Scanln(&opcion)
 
-			if opcion == 1 {
+		if opcion == 1 {
 
-				usuario.Registro()
+			usuario.Registro()
 
-			} else if opcion == 2 {
+		} else if opcion == 2 {
 
-				id_usuario, resultado := usuario.Login()	
+			id_usuario, resultado := usuario.Login()
 
-					if resultado == "El usuario es correcto, accediendo ..." {
-					for{
-						var opcion2 int
-						fmt.Println("Ecriba\n 1 para añadir un ingrediente a su inventario\n 2 para generar una receta\n 3 para cerrar sesión")
-						fmt.Scanln(&opcion2)
+			if resultado == "El usuario es correcto, accediendo ..." {
+				for {
+					var opcion2 int
+					fmt.Println("Ecriba\n 1 para añadir un ingrediente a su inventario\n 2 para generar una receta\n 3 para mostrar todas las refcetas \n 3 para cerrar sesión")
+					fmt.Scanln(&opcion2)
 
-						if opcion2 == 1 {
+					if opcion2 == 1 {
 
-							recetas.BuscarIngrediente(id_usuario)
-						}	
-						if opcion2 == 3{
-							break;
-						}
-				}
+						recetas.BuscarIngrediente(id_usuario)
 					}
-					
+					if opcion == 3 {
 
-			} else if opcion == 3{
-				
-				break
+					}
+					if opcion2 == 3 {
+						break
+					}
+				}
+			} else {
+				continue
 			}
+
+		} else if opcion == 3 {
+
+			break
 		}
+	}
 }
