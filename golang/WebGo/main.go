964e536955cc279a06f8fc6cc7c01888ccc99591
QuiqueCollada <e.collada@usp.ceu.es>
2020-11-25 10:25:18 +0100

    Formulario HTML

--- golang/WebGo/main.go
+++ golang/WebGo/main.go
@@ -1,7 +1,18 @@
-package main
+    package main
 
-import "net/http"
+    import (
+        "net/http"
 
-func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
-}
+        "github.com/russross/blackfriday"
+    )
+
+    func main () {
+        http.HandleFunc("/markdown", GeneraDesdeMarkdown)
+        http.Handle("/", http.FileServer(http.Dir("publico")))
+        http.ListenAndServe(":8080", nil)
+    }
+
+    func GeneraDesdeMarkdown(rw http.ResponseWriter, r *http.Request) {
+        html := blackfriday.MarkdownCommon([]byte (r.FormValue("cuerpo")))
+        rw.Write(html)
+    }
