7fbfd2bece3de001daae3754bfaf7925689b0d44
QuiqueCollada <e.collada@usp.ceu.es>
2020-11-25 12:33:29 +0100

    Cambios main app web

--- golang/WebGo/main.go
+++ golang/WebGo/main.go
@@ -1,18 +1,57 @@
-    package main
+package main
 
-    import (
-        "net/http"
+import (
+    "fmt"
+    "net/http"
 
-        "github.com/russross/blackfriday"
-    )
+    "github.com/gorilla/mux"
+)
 
-    func main () {
-        http.HandleFunc("/markdown", GeneraDesdeMarkdown)
-        http.Handle("/", http.FileServer(http.Dir("publico")))
-        http.ListenAndServe(":8080", nil)
-    }
+func main () {
+    r:. = mux.NewRouter()StrictSlash(false)
+    r.HandleFunc("/", ControladorInicio)
 
-    func GeneraDesdeMarkdown(rw http.ResponseWriter, r *http.Request) {
-        html := blackfriday.MarkdownCommon([]byte (r.FormValue("cuerpo")))
-        rw.Write(html)
-    }
+    // Colección de mensajes 
+    mensajes: = r.Path("/mensajes").Subrouter()
+    mensajes.Methods("GET").HandlerFunc(ControladorIndiceMensajes)
+    mensajes.Methods("POST").HandlerFunc(ControladorCreaMensajes)
+
+    // Mensaje singular 
+    mensaje: = r.PathPrefix("/mensajes/{id}").Subrouter()
+    mensaje.Methods("GET").Path("/editar").HandlerFunc(ControladorEditarMensaje)
+    mensaje.Methods("GET").HandlerFunc(ControladorMostrarMensaje)
+    mensaje.Methods("PUT", "POST").HandlerFunc(ControladorActualizaMensaje)
+    mensaje.Methods("DELETE").HandlerFunc(ControladorEliminaMensaje)
+
+    fmt.Println("Iniciando el servidor en :8080")
+    http.ListenAndServe(":8080", r)
+}
+
+func ControladorInicio(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "Inicio")
+}
+
+func ControladorIndiceMensajes(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "índice de mensajes")
+}
+
+func ControladorCreaMensajes(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "crear mensajes")
+}
+
+func ControladorMostrarMensaje(rw http.ResponseWriter, r *http.Request) {
+    id: = mux.Vars(r)["id"]
+    fmt.Fprintln(rw, "mostrando mensaje", id)
+}
+
+func ControladorActualizaMensaje(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "actualizando mensaje")
+}
+
+func ControladorEliminaMensaje(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "borrar mensaje")
+}
+
+func ControladorEditarMensaje(rw http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(rw, "editar mensaje")
+}
