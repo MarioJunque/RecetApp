65f6f732f544b648bbb1dd62d924bf58d73f2a2c
QuiqueCollada <e.collada@usp.ceu.es>
2020-12-13 10:47:57 +0100

    Creando sesión de usuario

--- golang/AppWeb/main.go
+++ golang/AppWeb/main.go
@@ -3,11 +3,50 @@ package main
 import (
     "html/template"
     "net/http"
-//    "fmt"
+    "github.com/gorilla/mux"
+    "github.com/gorilla/securecookie"
     "log"
     "github.com/MarioJunque/RecetApp/tree/master/golang/AppWeb/funciones"
 )
 
+
+var cookieHandler = securecookie.New(
+    securecookie.GenerateRandomKey(64),
+    securecookie.GenerateRandomKey(32))
+
+func getUserName(request *http.Request) (userName string) {
+    if cookie, err := request.Cookie("session"); err == nil {
+        cookieValue := make(map[string]string)
+        if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+            userName = cookieValue["name"]
+        }
+    }
+    return userName
+}
+
+func setSession(userName string, response http.ResponseWriter) {
+    value := map[string]string{
+        "name": userName,
+    }
+    if encoded, err := cookieHandler.Encode("session", value); err == nil {
+        cookie := &http.Cookie{
+            Name:  "session",
+            Value: encoded,
+            Path:  "/",
+        }
+        http.SetCookie(response, cookie)
+    }
+}
+
+func clearSession(response http.ResponseWriter) {
+    cookie := &http.Cookie{
+        Name:   "session",
+        Value:  "",
+        Path:   "/",
+        MaxAge: -1,
+    }
+    http.SetCookie(response, cookie)
+}
 
 func pantallaInicio(w http.ResponseWriter, r *http.Request) {
 
@@ -30,6 +69,7 @@ func internal(w http.ResponseWriter, r *http.Request) {
     r.ParseForm() 
 
 } 
+
 
 func main() {
     
