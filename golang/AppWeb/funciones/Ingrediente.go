37b617b40533511f86ea81de7ff84a630008505d
Sergio <sergiolopez382@gmail.com>
2020-12-19 17:38:30 +0100

    Código para manejar excepción de ingrediente inválido

--- golang/AppWeb/funciones/Ingrediente.go
+++ golang/AppWeb/funciones/Ingrediente.go
@@ -3,10 +3,11 @@ package funciones
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
+
 	_ "github.com/go-sql-driver/mysql"
-    "net/http"
-    "html/template"
 )
 
 type Ingrediente struct {
@@ -17,77 +18,74 @@ type Ingrediente struct {
 func AnnadirIngredienteAMiLista(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
-    tmpl := template.Must(template.ParseFiles("publico/annadirIngrediente.html"))   
-    tmpl.Execute(w, nil)
-    } else {
+		tmpl := template.Must(template.ParseFiles("publico/annadirIngrediente.html"))
+		tmpl.Execute(w, nil)
+	} else {
 
-    db, err := sql.Open("mysql", "root:root@/recetapp")
-	if err != nil {
-		log.Fatal("Cannot open DB connection", err)
-	}
-	defer db.Close()	
+		db, err := sql.Open("mysql", "root:root@/recetapp")
+		if err != nil {
+			log.Fatal("Cannot open DB connection", err)
+		}
+		defer db.Close()
 
-	redirectTarget := "/recetapp"
-	ingrediente:= Ingrediente {
-            Nombre:   r.FormValue("ingrediente"),
-        }        
-        
-    ingrediente.Id_ingrediente, err = ComprobarIngredienteBBDD(db, ingrediente.Nombre)
+		redirectTarget := "/ingredienteInvalido"
+		ingrediente := Ingrediente{
+			Nombre: r.FormValue("ingrediente"),
+		}
 
-    switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No está disponible este ingrediente en la base de datos")
-	case nil:
-		id_usuario := GetUserID(r)
-		id, err := Insert(db, ingrediente, id_usuario)
+		ingrediente.Id_ingrediente, err = ComprobarIngredienteBBDD(db, ingrediente.Nombre)
 
 		switch err {
 		case sql.ErrNoRows:
-			log.Fatal(id, err)
+			fmt.Println("No está disponible este ingrediente en la base de datos")
 		case nil:
-			redirectTarget = "/internal"
+			id_usuario := GetUserID(r)
+			id, err := Insert(db, ingrediente, id_usuario)
+
+			switch err {
+			case sql.ErrNoRows:
+				log.Fatal(id, err)
+			case nil:
+				redirectTarget = "/internal"
+			default:
+				panic(err)
+			}
+
 		default:
 			panic(err)
 		}
 
-	default:
-		panic(err)
-	}
-
-	http.Redirect(w, r, redirectTarget, 302)
+		http.Redirect(w, r, redirectTarget, 302)
 
 	}
 
 }
 
-
 //func BuscarIngrediente(id_usuario int) {
 
 func ComprobarIngredienteBBDD(db *sql.DB, nombreIngrediente string) (int, error) {
 
-
 	var ingrediente Ingrediente
-/*	stmt := "SELECT id_ingrediente FROM ingredientes WHERE nombre = ?"
-	fmt.Println(nombreIngrediente)
-	fmt.Println(stmt)
-	row := db.QueryRow(stmt,nombreIngrediente)
-	fmt.Println(row)
-	err := row.Scan(&ingrediente.Id_ingrediente)*/
-	row := db.QueryRow("SELECT id_ingrediente FROM ingredientes WHERE nombre = ?",nombreIngrediente)
-    err = row.Scan(&ingrediente.Id_ingrediente)
+	/*	stmt := "SELECT id_ingrediente FROM ingredientes WHERE nombre = ?"
+		fmt.Println(nombreIngrediente)
+		fmt.Println(stmt)
+		row := db.QueryRow(stmt,nombreIngrediente)
+		fmt.Println(row)
+		err := row.Scan(&ingrediente.Id_ingrediente)*/
+	row := db.QueryRow("SELECT id_ingrediente FROM ingredientes WHERE nombre = ?", nombreIngrediente)
+	err = row.Scan(&ingrediente.Id_ingrediente)
 	switch err {
 	case sql.ErrNoRows:
 		fmt.Println("No está disponible este ingrediente en la base de datos")
 		return -1, err
 	case nil:
 		return ingrediente.Id_ingrediente, err
-	default:		
+	default:
 		panic(err)
 	}
 }
 
 func Insert(db *sql.DB, ingrediente Ingrediente, id_usuario int) (int64, error) {
-
 
 	fmt.Println(id_usuario)
 	stmt, err := db.Prepare("INSERT INTO ingrediente_usuario (id_ingredientes, id_usuarios) VALUES(?,?)")
@@ -106,9 +104,9 @@ func Insert(db *sql.DB, ingrediente Ingrediente, id_usuario int) (int64, error) 
 
 func MostrarMisIngredientes(id_usuario int) {
 
-//	var ingrediente Ingrediente
+	//	var ingrediente Ingrediente
 
-		db, err := sql.Open("mysql", "root:root@/recetapp")
+	db, err := sql.Open("mysql", "root:root@/recetapp")
 	if err != nil {
 		log.Fatal("Cannot open DB connection", err)
 	}
@@ -120,40 +118,40 @@ func MostrarMisIngredientes(id_usuario int) {
 		log.Fatal(err)
 	} else {
 		fmt.Println(ingredientes)
-//		fmt.Println("%d\n",ingrediente.id_ingrediente)
-//    	fmt.Println("%s\n",ingrediente.nombre)
+		//		fmt.Println("%d\n",ingrediente.id_ingrediente)
+		//    	fmt.Println("%s\n",ingrediente.nombre)
 	}
 }
 
 func ObtenerMisIngredientes(db *sql.DB, id_usuario int) ([]Ingrediente, error) {
 
 	var ingrediente Ingrediente
-    var ingredientes []Ingrediente
-    stmt := "select id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios = ?"
+	var ingredientes []Ingrediente
+	stmt := "select id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios = ?"
 
 	rows, err := db.Query(stmt, id_usuario)
 
-//    rows, err := db.Query("select id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios=?;", id_usuario)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
+	//    rows, err := db.Query("select id_ingredientes, nombre from ingrediente_usuario, ingredientes where ingrediente_usuario.id_ingredientes=ingredientes.id_ingrediente AND id_usuarios=?;", id_usuario)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        err := rows.Scan(&ingrediente.Id_ingrediente,&ingrediente.Nombre)
-        if err != nil {
-            log.Fatal(err)
-        }
+	for rows.Next() {
+		err := rows.Scan(&ingrediente.Id_ingrediente, &ingrediente.Nombre)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-//    ingredientes = append(ingredientes, Ingrediente{id_ingrediente: ingrediente.id_ingrediente, nombre: ingrediente.nombre})
-	ingredientes = append(ingredientes, ingrediente)
+		//    ingredientes = append(ingredientes, Ingrediente{id_ingrediente: ingrediente.id_ingrediente, nombre: ingrediente.nombre})
+		ingredientes = append(ingredientes, ingrediente)
 
-    }
-    if err := rows.Err(); err != nil {
-        log.Fatal(err)
-    }
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    return ingredientes, err
+	return ingredientes, err
 }
 
 func BorrarIngrediente(db *sql.DB, id_ingrediente int, id_usuario int) {
@@ -175,4 +173,3 @@ func ComprobarIngrediente(db *sql.DB, id_ingrediente int, nombreIngrediente stri
 
 	return 1
 }
-
--- golang/AppWeb/main.go
+++ golang/AppWeb/main.go
@@ -1,62 +1,68 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
-    "log"
-    "github.com/MarioJunque/RecetApp/tree/master/golang/AppWeb/funciones"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/MarioJunque/RecetApp/tree/master/golang/AppWeb/funciones"
 )
-
-
 
 func pantallaInicio(w http.ResponseWriter, r *http.Request) {
 
-    fs := http.FileServer(http.Dir("publico"))
-    http.Handle("/publico/", http.StripPrefix("/publico/", fs))
-    tmpl := template.Must(template.ParseFiles("publico/index.html"))   
-    tmpl.Execute(w, nil)   
-    r.ParseForm() 
+	fs := http.FileServer(http.Dir("publico"))
+	http.Handle("/publico/", http.StripPrefix("/publico/", fs))
+	tmpl := template.Must(template.ParseFiles("publico/index.html"))
+	tmpl.Execute(w, nil)
+	r.ParseForm()
 
-}    
-
+}
 
 func internal(w http.ResponseWriter, r *http.Request) {
 
-//    fs := http.FileServer(http.Dir("publico"))
-//    http.Handle("/publico/", http.StripPrefix("/publico/", fs))
-    tmpl := template.Must(template.ParseFiles("publico/internal.html"))   
-    tmpl.Execute(w, nil)
+	//    fs := http.FileServer(http.Dir("publico"))
+	//    http.Handle("/publico/", http.StripPrefix("/publico/", fs))
+	tmpl := template.Must(template.ParseFiles("publico/internal.html"))
+	tmpl.Execute(w, nil)
 
-    r.ParseForm() 
+	r.ParseForm()
 
 }
 
 func annadirIngrediente(w http.ResponseWriter, r *http.Request) {
 
-//    fs := http.FileServer(http.Dir("publico"))
-//    http.Handle("/publico/", http.StripPrefix("/publico/", fs))
-    tmpl := template.Must(template.ParseFiles("publico/annadirIngrediente.html"))   
-    tmpl.Execute(w, nil)
+	//    fs := http.FileServer(http.Dir("publico"))
+	//    http.Handle("/publico/", http.StripPrefix("/publico/", fs))
+	tmpl := template.Must(template.ParseFiles("publico/annadirIngrediente.html"))
+	tmpl.Execute(w, nil)
 
-    r.ParseForm() 
+	r.ParseForm()
 
-}  
+}
 
+func ingredienteNoValido(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("publico/ingredienteInvalido.html"))
+	tmpl.Execute(w, nil)
+
+	r.ParseForm()
+}
 
 func main() {
-    
-    http.HandleFunc("/recetapp", pantallaInicio)
-    http.HandleFunc("/login", funciones.Login)
-    http.HandleFunc("/internal", internal)
-    http.HandleFunc("/registro", funciones.Registro)
-    http.HandleFunc("/AnnadirIngrediente", annadirIngrediente)
-    http.HandleFunc("/ingrediente", funciones.AnnadirIngredienteAMiLista)
-	http.HandleFunc("/logout", funciones.Login)
-    http.HandleFunc("/recetas", funciones.MostrarMisRecetas)
-//    http.HandleFunc("/receta",funciones.Receta)
 
-    err := http.ListenAndServe(":8080", nil) // setting listening port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/recetapp", pantallaInicio)
+	http.HandleFunc("/login", funciones.Login)
+	http.HandleFunc("/internal", internal)
+	http.HandleFunc("/registro", funciones.Registro)
+	http.HandleFunc("/AnnadirIngrediente", annadirIngrediente)
+	http.HandleFunc("/ingrediente", funciones.AnnadirIngredienteAMiLista)
+	http.HandleFunc("/ingredienteInvalido", ingredienteNoValido)
+	http.HandleFunc("/logout", funciones.Login)
+	http.HandleFunc("/recetas", funciones.MostrarMisRecetas)
+	//    http.HandleFunc("/receta",funciones.Receta)
+
+	err := http.ListenAndServe(":8080", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
